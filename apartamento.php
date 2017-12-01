24b25e5fe23d211e5ba44381ea34c73079322689
Julian Patiño <juliodeoro@gmail.com>
2017-11-30 23:59:16 -0500

    ...

--- apartamento.php
+++ apartamento.php
@@ -5,12 +5,12 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, height=device-height">
+		 <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 		<title>Apartamentos Horizonte Verde</title>
 
-		<!-- Latest compiled and minified CSS -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-		<link rel="stylesheet" href="css/style.css">
+		<link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+  		<link href="https://unpkg.com/vuetify/dist/vuetify.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css"> 
 		<link rel="stylesheet" href="css/iphone-responsive.css">
 
 
@@ -18,7 +18,7 @@
 
 	<body>
 		<div id="app">
-			<div id="first-section">
+			<v-layout row wrap id="first-section" v-show="showprimero">
 				<div class="info-apartment">
 					<p class="titlehome"><a href="index.html"><i class="fa fa-home btn-go-home" aria-hidden="true"></i></a></p>
 					<div class="text-info-container">
@@ -27,43 +27,33 @@
 						<h3>{{header.subtitle}}</h3>
 					</div>
 					<div class="capsule-button">
-						<button class="button button-seemore content-switch" data-target-spy="#second-section" data-timelaps="1500">VER DETALLE</button>
+						<button class="button button-seemore content-switch" id="btnBackBottom" v-on:click="changeView()">VER DETALLE</button>
 					</div>
 				</div>
-			</div>
-			<div class="container hide-scroll" id="second-section">
-				<div class="row">
+			</v-layout>
+			<v-layout row wrap id="second-section" v-show="showsegundo">
+				<v-flex>
 					<header>
 						<div class="header-navigation">
 							<div class="wrap">
 								<div class="capsule-button">
 									<a href="index.html"><i class="fa fa-home btn-go-home" aria-hidden="true"></i></a>
-									<button class="button btn-go-back content-switch" data-target-spy="#first-section" data-timelaps="1500">VOLVER</button>
-									<div class="navbar-header">
-										<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-											<span class="icon-bar"></span>
-											<span class="icon-bar"></span>
-											<span class="icon-bar"></span>
-										</button>
-									</div>
+									<button class="button btn-go-back content-switch" id="btnBackTop" v-on:click="changeView()">VOLVER</button>
 								</div>
 							</div>
 						</div>
-						 <nav class="wrap">
-						    <div class="collapse navbar-collapse" id="myNavbar">
-								<ul class="nav navbar-nav">
-									<li><a data-toggle="pill" href="#informacion-tab">INFORMACI&Oacute;N</a></li> 
-									<li><a data-toggle="pill" href="#interiores-tab">INTERIORES</a></li>
-									<li><a data-toggle="pill" href="#plantas-tab">PLANTAS AMBIENTADAS</a></li>
-								</ul>
-						    </div>
-						</nav> 
 					</header>
-					<section>
-						<div class="tab-content ">
-							<div class="tab-pane active" id="informacion-tab">
-								<div class="wrap row">
-									<div class="col-md-4">
+				    <v-tabs>
+						<v-tabs-bar class="navbar">
+							<v-tabs-slider class="yellow"></v-tabs-slider>
+							<v-tabs-item xs12 href="#informacion-tab">INFORMACI&Oacute;N</v-tabs-item> 
+							<v-tabs-item xs12 href="#interiores-tab">INTERIORES</v-tabs-item>
+							<v-tabs-item xs12 href="#plantas-tab">PLANTAS AMBIENTADAS</v-tabs-item>
+						</v-tabs-bar>
+						<v-tabs-items>
+							<v-tabs-content id="informacion-tab">
+								<v-layout wrap row>
+									<v-flex offset-md1 xs12 sm12 md4>
 										<h3 class="info-text-green">{{info.titulo}}</h3>
 										<p class="texto-h4" v-html="info.infogeneral"></p>
 
@@ -76,102 +66,52 @@
 										<div class="col-md-12" style="text-align: center; margin-top: 20px;">
 											<img src="images/vistaCordilleraApart.jpg"/>
 										</div>
-									</div>
-									<div class="col-md-4">
+									</v-flex>
+									<v-flex xs12 sm12 md4>
 										<img class="img-recorridos img-responsive" :src="info.img_apart" />
-									</div>
-									<div class="col-md-4">
+									</v-flex>
+									<v-flex xs12 sm12 md2>
 										<div id="torresApart1Container" class="torres-images">
-											<div id="id_backT" onMouseDown="backT()"></div>
+											<div id="id_backT"></div>
 											<div class="apartamento-dual" :style="{backgroundImage: `url(${info.imagenestorresarr})`}"></div>
-											<div id="id_nextT" onMouseDown="nextT()"></div>
+											<div id="id_nextT"></div>
 										</div>
 										
 										<div style="text-align: center; margin-top: 20px;">
 											<div class="available"></div>
 											<div id="list_apart1" style="font-size:160%;"></div>
 										</div>
-									</div>
-								</div>
-							</div>
+									</v-flex>
+								</v-layout>
+							</v-tabs-content>
 
-							<div class="tab-pane" id="interiores-tab">
+							<v-tabs-content id="interiores-tab">
 
-								<div id="container2a">
+								<v-carousel >
+									<v-carousel-item src="images/apartments/apart1/01.jpg"></v-carousel-item>
+									<v-carousel-item src="images/apartments/apart1/02.jpg"></v-carousel-item>
+									<v-carousel-item src="images/apartments/apart1/03.jpg"></v-carousel-item>
+								</v-carousel>
 
+							</v-tabs-content>
 
-									<div id="divImgInteriores">
+							<v-tabs-content id="plantas-tab">
+								<d-block>
+									<v-carousel >
+										<v-carousel-item v-for="image in info.imagenesslider" :src=`images/tipos/${image.url}`></v-carousel-item>
+									</v-carousel>
+								</d-block>
+							</v-tabs-content>
 
-
-
-										<div id="myCarousel2" class="carousel slide" data-ride="carousel">
-											<!-- Indicators -->
-											<ol class="carousel-indicators">
-												<li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
-												<li data-target="#myCarousel2" data-slide-to="1"></li>
-												<li data-target="#myCarousel2" data-slide-to="2"></li>
-											</ol>
-
-											<!-- Wrapper for slides -->
-											<div class="carousel-inner">
-												<div class="item active">
-													<img src="images/apartments/apart1/01.jpg">
-												</div>
-
-												<div class="item">
-													<img src="images/apartments/apart1/02.jpg">
-												</div>
-
-												<div class="item">
-													<img src="images/apartments/apart1/03.jpg">
-												</div>
-											</div>
-
-											<!-- Left and right controls -->
-										    <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
-										      <span class="glyphicon glyphicon-chevron-left"></span>
-										      <span class="sr-only">Previous</span>
-										    </a>
-										    <a class="right carousel-control" href="#myCarousel2" data-slide="next">
-										      <span class="glyphicon glyphicon-chevron-right"></span>
-										      <span class="sr-only">Next</span>
-										    </a>
-										</div>
-
-									</div>
-
-
-								</div>
-							</div>
-
-							<div class="tab-pane" id="plantas-tab">
-								<div class="wrap nav-slider-pisos">
-									<ul class="nav nav-pills nav-justified" v-for="(imagetitle, index) in info.imagenesslider">
-										<li><a data-target="#myCarousel" data-slide-to="{{index}}">{{imagetitle.titulo}}</a></li>
-									</ul>
-								</div>
-
-								<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
-
-									<!-- Wrapper for slides -->
-									<div class="carousel-inner">
-										<div class="item active" v-for="image in info.imagenesslider">
-											<img :src=`images/tipos/${image.url}`>
-										</div>
-									</div>
-								</div>
-							</div>
-
-						</div>
-					</section>
-				</div>
-			</div>
+						</v-tabs-items>
+					</v-tabs>
+				</v-flex>
+			</v-layout>
 		</div>
-		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/scroll-page.js"></script> 
 		<script type="text/javascript" src="https://unpkg.com/vue@2.5.8/dist/vue.js"></script>
+		<script src="https://unpkg.com/vuetify/dist/vuetify.js"></script>
 		<script type="text/javascript" src="js/main-vue.js"></script>
+		<script type="text/javascript" src="js/scroll-page.js"></script> 
 	</body>
 
 </html>
--- js/scroll-page.js
+++ js/scroll-page.js
@@ -1,64 +1,15 @@
-function iniciarScrollSpy() {
-	var firstContainer = $("#first-section");
-	var secondContainer = $("#second-section");
+(function () {
 
-	$('html, body').animate({scrollTop: firstContainer.offset().top});
-	document.querySelectorAll(".content-switch").forEach(ele =>{
-		ele.addEventListener("click", (e) =>{
-			let dataButton = e.currentTarget.dataset;
-			$('html, body').animate({
-	            scrollTop: $(dataButton.targetSpy).offset().top
-	        }, {
-	        	duration : dataButton.timelaps,
-	        	start: () => {
-	        		let eleTriggered = (e.currentTarget.targetSpy == "#second-section") ? $("#second-section") : $("#first-section") ;
-	        		$(e.currentTarget.dataset.targetSpy).removeClass("hide-scroll");
-	        		eleTriggered.addClass("hide-scroll");
-	        	}
-	        });
-		});
-	});
-
-	let imagenTorreApartamentoContainer = $("#torresApart1Container").find(".apartamento-dual");
+	let imagenTorreApartamentoContainer = document.getElementById("torresApart1Container").querySelector(".apartamento-dual");
 
 
 	document.getElementById("id_backT").addEventListener("click", () => {
-		imagenTorreApartamentoContainer.toggleClass("move-right")
+		imagenTorreApartamentoContainer.classList.toggle("move-right")
 	});
 
 	document.getElementById("id_nextT").addEventListener("click", () => {
-		imagenTorreApartamentoContainer.toggleClass("move-right")
-	});
-
-	let navPills = $(".nav-pills.nav-justified");
-
-	navPills.find("a").on("click", (e) => {
-		navPills.find(".active").removeClass("active");
-		$(e.currentTarget.parentNode).addClass("active");
+		imagenTorreApartamentoContainer.classList.toggle("move-right")
 	});
 
 
-	let params = (new URL(document.location)).searchParams;
-	/*var request = $.ajax({
-		url: "controllers/get/getApartamentos.php",
-		method: "POST",
-		data: { id : params.get("id"), torre : params.get("torre") },
-		dataType: "json"
-	});
-
-	request.done(function( dataResponse ) {
-		let strApart = ""; 
-		for(i in dataResponse){
-			strApart += (dataResponse.length > 1 && i < (dataResponse.length-1)) ? dataResponse[i].idApart + " - " : dataResponse[i].idApart;
-		}
-		$("#list_apart1").text(strApart)
-	});
-	 
-	request.fail(function( jqXHR, textStatus ) {
-	  console.log( "Request failed: " + textStatus );
-	});*/
-
-}
-
-
-$(document).ready(iniciarScrollSpy);
+})();
