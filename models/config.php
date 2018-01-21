4042325104bdaa72e6b925e76c52f91b6933eaef
julian Otalvaro Osorio <jotalvaro@arvirt.com>
2018-01-21 20:54:03 +0000

    ...

--- models/config.php
+++ models/config.php
@@ -1,13 +1,13 @@
 <?php
-    define('DB_HOST','localhost');
+/*    define('DB_HOST','localhost');
     define('DB_USER','root');
     define('DB_PASS','root');
     define('DB_NAME','horizonteverde');
-    define('DB_CHARSET','utf8');
+    define('DB_CHARSET','utf8');*/
 
-/*    define('DB_HOST','localhost');
+    define('DB_HOST','localhost');
     define('DB_USER','arvir49_admin');
     define('DB_PASS','producto45682016');
     define('DB_NAME','arvir49_producto');
-    define('DB_CHARSET','utf8');*/
+    define('DB_CHARSET','utf8');
 ?> 
