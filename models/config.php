64fb640f7b873c10e74f7ccac81f206727ff47f2
Julian Alberto Patiño <juliodeoro@gmail.com>
2018-01-21 16:00:34 -0500

    Merge master into master

--- models/config.php
+++ models/config.php
@@ -2,13 +2,8 @@
 /*    define('DB_HOST','localhost');
     define('DB_USER','root');
     define('DB_PASS','root');
-<<<<<<< HEAD
-    define('DB_NAME','horizonteverde');
-    define('DB_CHARSET','utf8');*/
-=======
     define('DB_NAME','arvir49_producto');
-    define('DB_CHARSET','utf8');
->>>>>>> 6cc058cd76032c3b8a1cf9ae59acb428c412639e
+    define('DB_CHARSET','utf8');*/
 
     define('DB_HOST','localhost');
     define('DB_USER','arvir49_admin');
